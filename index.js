de21dc39e5b1d758cded6bba84fd64254a3712b1
nachoz12341 <40530732+nachoz12341@users.noreply.github.com>
2019-06-14 13:41:12 -0500

    Revert "added test api call"
    This reverts commit c8da3ef317fb1e8c3bc9af7f2b77c955c2f0b149.

--- index.js
+++ index.js
@@ -2,22 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    //response.end("Hello World!");
-
-    https.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=0eefa07bb1bb480bad3277dcfc313086', (res) => {
-                var body = '';
-
-                res.on('data', (d) =>{body+=d;});
-                res.on('end', () =>{
-                    var json = JSON.parse(body);
-                    var message = 'Here is some more information. '+json.articles[articleNum].description+'. If you would like to hear more news, simply ask. You can specify country, topic or source.'; 
-
-                    response.end(message);
-                });
-                res.on('error',(e) =>{
-                    console.log(e);
-                });
-    });
+    response.end("Hello World!");
 });
 
 const port = process.env.PORT || 1337;
