c54180fbf336483358901825dbae6a230ef48878
Ignacio Montes de Oca <nachoz12341@gmail.com>
2019-06-24 09:34:07 -0500

    Fixed wrong function for sending response

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 app.get('/',(req,res) =>{
     let webhookResponse = {
         "fulfillmentText": "Sent a response from Azure!"
-    }
+    };
 
-    app.send(webhookResponse);
+    res.send(webhookResponse);
 });
 
 const port = process.env.PORT || 1337;
