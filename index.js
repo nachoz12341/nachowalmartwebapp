62a744a51958854d4e0fef1cf798735d444723cd
Ignacio Montes de Oca <nachoz12341@gmail.com>
2019-06-24 13:10:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.post('/', (req, res) => {
     req.on('end', () => {
         let session = JSON.parse(data);        
 
-        res.send({ "fulfillmentText": JSON.stringify(session)});
+        res.send({ "fulfillmentText": JSON.stringify(session.intent)});
     });
 });
 
