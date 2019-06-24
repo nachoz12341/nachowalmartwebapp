b6283246288f1769879c74a8172f847149e6a69c
Ignacio Montes de Oca <nachoz12341@gmail.com>
2019-06-24 15:07:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.post('/', (req, res) => {
     req.on('end', () => {
 
         console.log("\n---------------------------\n");
-        console.log(req.body);
+        console.log(req.params);
         console.log("\n---------------------------\n");
 
         const session = JSON.parse(data);
