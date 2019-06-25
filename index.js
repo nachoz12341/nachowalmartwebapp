540aef1567c20a54a3ae1a1e46b179ae5aef88d9
Nacho Montes de Oca <ium000k@HOMEOFFICE.wal-mart.com>
2019-06-25 13:18:07 -0500

    removed comment

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//Answer dialogflow post request
 app.post('/', (req, res) => {
     const intentName = req.body.queryResult.intent.displayName;
     const parameters = req.body.queryResult.parameters;
